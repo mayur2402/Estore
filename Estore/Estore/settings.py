b815442aa38682c08df89c5f9a68c9efe95ba269
mayur2402 <59564042+mayur2402@users.noreply.github.com>
2020-12-26 15:08:12 +0530

    Update settings.py

--- Estore/Estore/settings.py
+++ Estore/Estore/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mayurdimble412105@gmail.com'
-EMAIL_HOST_PASSWORD = 'lenovoa2010@'
+EMAIL_HOST_USER = 'sendermail@gmail.com'
+EMAIL_HOST_PASSWORD = 'senderpassword'
